acec038a9748f7ec8a036586f342199cbede6280
aqilabihar <aqilabihar123@gmail.com>
2026-04-27 19:22:59 +0700

    add fungtional lidar camera simulation 3d

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -17,27 +17,30 @@ def generate_launch_description():
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name='suraqil_bot'
+    package_name = 'suraqil_bot'
+    world_file = os.path.join(get_package_share_directory(package_name), 'worlds', 'obstacle_test.world')
 
+    # Robot State Publisher
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the ros_gz_sim package
+    # Gazebo Sim
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                    launch_arguments={'gz_args': '-r empty.sdf'}.items(),
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
+        )]), launch_arguments={'gz_args': ['-r ', world_file]}.items(),
     )
 
-    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='ros_gz_sim', executable='create',
-                        arguments=['-topic', 'robot_description',
-                                   '-name', 'my_bot',
-                                   '-z', '0.05'],
-                        output='screen')
+    # Spawn Robot
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-topic', 'robot_description', '-name', 'my_bot', '-z', '0.1'],
+        output='screen'
+    )
 
     # Create the bridge for cmd_vel and odom
     bridge = Node(
@@ -47,7 +50,8 @@ def generate_launch_description():
                    '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
                    '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
                    '/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model',
-                   '/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V'],
+                   '/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
+                   '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan'],
         output='screen'
     )
 
